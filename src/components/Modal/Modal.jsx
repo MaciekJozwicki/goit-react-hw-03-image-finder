409380aded4bef04dfd2b0f89358a0db9ea0fcf0
Maciek <maciekjozwicki@wp.pl>
2023-12-21 13:36:37 +0100

    Update Modal.jsx

--- src/components/Modal/Modal.jsx
+++ src/components/Modal/Modal.jsx
@@ -3,13 +3,16 @@ import PropTypes from 'prop-types';
 import styles from './Modal.module.css';
 
 class Modal extends Component {
+  handleEscapeKey = e => {
+    if (e.key === 'Escape') {
+      this.props.closeModal();
+    }
+  };
   componentDidMount() {
-    document.addEventListener('keydown', e => {
-      if (e.key === 'Escape') {
-        this.props.closeModal();
-        document.removeEventListener('keydown');
-      }
-    });
+    document.addEventListener('keydown', this.handleEscapeKey);
+  }
+  componentWillUnmount() {
+    document.removeEventListener('keydown', this.handleEscapeKey);
   }
   render() {
     const { url, alt } = this.props.imageObject;
